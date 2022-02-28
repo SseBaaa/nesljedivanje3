a8bdbe9e514fa64c75f59cda83e2c789efff64c1
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-02-28 17:24:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanje_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno hrast = new Bjelogoricno();
+            Crnogoricno bor = new Crnogoricno();
+
+            Console.WriteLine($"Hrast : {hrast.OtpadajuListovi}");
+            Console.WriteLine($"Bor : {bor.OtpadajuListovi}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Biljka
+    {
+
+    }
+
+    public class Stablo : Biljka
+    {
+        public bool otpadajuListovi;
+
+        public Stablo(bool otpadajuListovi)
+        {
+            otpadajuListovi = OtpadajuListovi;
+        }
+
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+    }
+
+    public class Cvijet : Biljka
+    {
+
+    }
+
+    public class Bjelogoricno : Stablo
+    {
+      public Bjelogoricno(bool otpadajuListovi = true) : base(otpadajuListovi)
+        {
+            OtpadajuListovi = true;
+        }
+
+    }
+
+    public class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool otpadajuListovi = false) : base(otpadajuListovi)
+        {
+
+        }
+
+    }
+
+
+}
